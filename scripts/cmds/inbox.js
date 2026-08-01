09ef0bcb029f66b200ea59b9bdd5dfa8238a7cff
siyamxpro <dvx9076@gmail.com>
2026-08-01 21:27:22 +0600

    Update inbox.js

--- scripts/cmds/inbox.js
+++ scripts/cmds/inbox.js
@@ -1,37 +1,37 @@
 module.exports = {
   config: {
-      name: "inbox",
-          aliases: ["in"],
-              version: "1.0",
-                  author: "FARHAN-KHAN",
-                      countDown: 10,
-                          role: 0,
-                              shortDescription: {
-                                    en: "hello goatbot inbox no prefix file enjoy the cmmand"
-                                        },
-                                            longDescription: {
-                                                  en: ""
-                                                      },
-                                                          category: "fun",
-                                                              guide: {
-                                                                    en: ""
-                                                                        }
-                                                                          },
-                                                                            langs: {
-                                                                                en: {
-                                                                                      gg: ""
-                                                                                          },
-                                                                                              id: {
-                                                                                                    gg: ""
-                                                                                                        }
-                                                                                                          },
-                                                                                                            onStart: async function({ api, event, args, message }) {
-                                                                                                                try {
-                                                                                                                      const query = encodeURIComponent(args.join(' '));
-                                                                                                                            message.reply("✅ SUCCESSFULLY SEND MSG\n\n🙃জানু তোমার ইনবক্স চেক করো , দেখো প্রপোজ করেছি,🐸🫣", event.threadID);
-                                                                                                                                  api.sendMessage("✅ SUCCESSFULLY ALLOW\n😁 জানু  ইনবক্স এ আসতে বললে কেনো ,কি বলবে বলো 🙂", event.senderID);
-                                                                                                                                      } catch (error) {
-                                                                                                                                            console.error("Error bro: " + error);
-                                                                                                                                                }
-                                                                                                                                                  }
-                                                                                                                                                  }
+    name: "inbox",
+    aliases: ["in"],
+    version: "1.0",
+    author: "𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍",
+    countDown: 10,
+    role: 0,
+    shortDescription: {
+      en: "hello goatbot inbox no prefix file enjoy the cmmand"
+    },
+    longDescription: {
+      en: ""
+    },
+    category: "fun",
+    guide: {
+      en: ""
+    }
+  },
+  langs: {
+    en: {
+      gg: ""
+    },
+    id: {
+      gg: ""
+    }
+  },
+  onStart: async function ({ api, event, args, message }) {
+    try {
+      const query = encodeURIComponent(args.join(' '));
+      message.reply("✅ 𝐒𝐔𝐂𝐂𝐄𝐒𝐒𝐅𝐔𝐋𝐋𝐘 𝐒𝐄𝐍𝐃 𝐌𝐒𝐆\n\n🙃জানু তোমার ইনবক্স চেক করো , দেখো প্রপোজ করেছি,🐸🫣", event.threadID);
+      api.sendMessage("✅ 𝐒𝐔𝐂𝐂𝐄𝐒𝐒𝐅𝐔𝐋𝐋𝐘 𝐀𝐋𝐋𝐎𝐖\n😁 জানু  ইনবক্স এ আসতে বললে কেনো ,কি বলবে বলো 🙂", event.senderID);
+    } catch (error) {
+      console.error("Error bro: " + error);
+    }
+  }
+};
