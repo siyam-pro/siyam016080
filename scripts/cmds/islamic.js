66e1e35749a00fc7f7958331b266e7b4339dc3b0
siyamxpro <dvx9076@gmail.com>
2026-08-01 21:24:55 +0600

    Update islamic.js

--- scripts/cmds/islamic.js
+++ scripts/cmds/islamic.js
@@ -6,11 +6,11 @@ module.exports = {
   config: {
     name: "islamic",
     version: "2.0.0",
-    author: "FARHAN-KHAN",
+    author: "𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍",
     countDown: 5,
     role: 0,
-    shortDescription: "Sad video sender 😢",
-    longDescription: "Sends random sad video with emotional captions 💔",
+    shortDescription: "Islamic video sender 🕋",
+    longDescription: "Sends random Islamic video with emotional captions 🕋",
     category: "media",
     guide: {
       en: "{pn}"
@@ -18,40 +18,40 @@ module.exports = {
   },
 
   onStart: async function ({ api, event }) {
-    // 💔 Random sad captions
+    // 💔 Random captions
     const captions = [
       "===「𝐏𝐑𝐄𝐅𝐈𝐗-𝐄𝐕𝐄𝐍𝐓」=== \n--❖(✷‿𝐍𝐈𝐉𝐇𝐔𝐌-𝐁𝐎𝐓‿✷)❖-- \n✢━━━━━━━━━━━━━━━✢        \n🕋 ♡-𝐈𝐒𝐋𝐀𝐌𝐈𝐂-𝐕𝐈𝐃𝐄𝐎-♡ 🕋 \n✢━━━━━━━━━━━━━━━✢\n(✷‿𝐎𝐖𝐍𝐄𝐑:-‿𝐃-𝐒 𝐒𝐈𝐘𝐀𝐌‿✷)"
     ];
 
     const caption = captions[Math.floor(Math.random() * captions.length)];
 
-    // 🎥 Sad videos list
+    // 🎥 Videos list
     const links = [
-    "https://drive.google.com/uc?id=14emH_6vF3fuJe2vmeC52e575TppboHne",
-    "https://drive.google.com/uc?id=15APJbSuGLY7zCiZsAgU7HjCJeinYDX9K",
-    "https://drive.google.com/uc?id=15ImMIXM_mqPM8hXpQNPLTGCrm9sh0RPS",
-    "https://drive.google.com/uc?id=14qUnMm3J3cUqImDDy4ehRjDiv_NeRpMo",
-    "https://drive.google.com/uc?id=15ZqanDuEYrC-lHSsiIYAjWagr1h8yZpP",
-    "https://drive.google.com/uc?id=155rlKywUHP3xzgJkQ1ztxXpKnDxXtXlb",
-    "https://drive.google.com/uc?id=156MaTKck-_ureBfj7NI-iU7_rGut-ssD",
-    "https://drive.google.com/uc?id=15l4gxljfoe9-WvQKzffjambLC5Tt1YNd",
-    "https://drive.google.com/uc?id=15fauLjjElJ0loxajhUvDeaKTqW4YdskK",
-    "https://drive.google.com/uc?id=16IBAHr7AlKM1RR4hiTBuvAn5x27ed6j4",
-    "https://drive.google.com/uc?id=15amvNN6WLIKwg17ufgFhs7EqI0EXNxy5",
-    "https://drive.google.com/uc?id=15OS5gFi2QGZm5TTStIn6iD3YRUNHw1Zm",
-    "https://drive.google.com/uc?id=168qMjWaEyObyBgJrilyTb4vOcvgynQAD",
-    "https://drive.google.com/uc?id=15FFHINVpAbr4ykjkhk1_vQ5uDQakTcpy",
-    "https://drive.google.com/uc?id=14j501R3TheTH3YLInLZlLTU-oXVvjegw",
-    "https://drive.google.com/uc?id=15UmCBW1ddt6Kpt9xytqPpXiJip-05bDG",
-    "https://drive.google.com/uc?id=14e0lCDG6vwzGi8apiDcm38Wov911501y",
-    "https://drive.google.com/uc?id=15Cbl-YGajKcV0QMp6bDtRT4dI-K6lWR0",
-    "https://drive.google.com/uc?id=15hJ9St2amhdLnowAvuDn0BicgZ5Aw0rW",
-    "https://drive.google.com/uc?id=15QIjrXblGNjf5b3J6dRQ4XMSV-_j7soB",
-    "https://drive.google.com/uc?id=15tgfSnX-ICfO8V5T6vXbb_AwYkfl_EYX"
+      "https://drive.google.com/uc?id=14emH_6vF3fuJe2vmeC52e575TppboHne",
+      "https://drive.google.com/uc?id=15APJbSuGLY7zCiZsAgU7HjCJeinYDX9K",
+      "https://drive.google.com/uc?id=15ImMIXM_mqPM8hXpQNPLTGCrm9sh0RPS",
+      "https://drive.google.com/uc?id=14qUnMm3J3cUqImDDy4ehRjDiv_NeRpMo",
+      "https://drive.google.com/uc?id=15ZqanDuEYrC-lHSsiIYAjWagr1h8yZpP",
+      "https://drive.google.com/uc?id=155rlKywUHP3xzgJkQ1ztxXpKnDxXtXlb",
+      "https://drive.google.com/uc?id=156MaTKck-_ureBfj7NI-iU7_rGut-ssD",
+      "https://drive.google.com/uc?id=15l4gxljfoe9-WvQKzffjambLC5Tt1YNd",
+      "https://drive.google.com/uc?id=15fauLjjElJ0loxajhUvDeaKTqW4YdskK",
+      "https://drive.google.com/uc?id=16IBAHr7AlKM1RR4hiTBuvAn5x27ed6j4",
+      "https://drive.google.com/uc?id=15amvNN6WLIKwg17ufgFhs7EqI0EXNxy5",
+      "https://drive.google.com/uc?id=15OS5gFi2QGZm5TTStIn6iD3YRUNHw1Zm",
+      "https://drive.google.com/uc?id=168qMjWaEyObyBgJrilyTb4vOcvgynQAD",
+      "https://drive.google.com/uc?id=15FFHINVpAbr4ykjkhk1_vQ5uDQakTcpy",
+      "https://drive.google.com/uc?id=14j501R3TheTH3YLInLZlLTU-oXVvjegw",
+      "https://drive.google.com/uc?id=15UmCBW1ddt6Kpt9xytqPpXiJip-05bDG",
+      "https://drive.google.com/uc?id=14e0lCDG6vwzGi8apiDcm38Wov911501y",
+      "https://drive.google.com/uc?id=15Cbl-YGajKcV0QMp6bDtRT4dI-K6lWR0",
+      "https://drive.google.com/uc?id=15hJ9St2amhdLnowAvuDn0BicgZ5Aw0rW",
+      "https://drive.google.com/uc?id=15QIjrXblGNjf5b3J6dRQ4XMSV-_j7soB",
+      "https://drive.google.com/uc?id=15tgfSnX-ICfO8V5T6vXbb_AwYkfl_EYX"
     ];
 
     const link = links[Math.floor(Math.random() * links.length)];
-    const cachePath = path.join(__dirname, "cache", "sad.mp4");
+    const cachePath = path.join(__dirname, "cache", "islamic.mp4");
 
     try {
       const response = await axios({
