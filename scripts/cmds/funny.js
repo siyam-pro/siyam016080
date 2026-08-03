538fb4a11dda91281661fe5988353e528efc86ca
siyamxpro <dvx9076@gmail.com>
2026-08-03 22:33:37 +0600

    Update funny.js

--- scripts/cmds/funny.js
+++ scripts/cmds/funny.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "funny",
     version: "2.0.0",
-    author: "FARHAN-KHAN",
+    author: "𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍",
     countDown: 5,
     role: 0,
     shortDescription: "Funny video sender 😃",
