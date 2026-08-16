3b22b26e87806bf03ffa7b3941a11c9ed0cb0ef5
siyamxpro <dvx9076@gmail.com>
2026-08-16 14:53:39 +0600

    Update webss.js

--- scripts/cmds/webss.js
+++ scripts/cmds/webss.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "webss",
     version: "1.1",
-    author: "MR_FARHAN",
+    author: "𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍",
     countDown: 5,
     role: 0,
     shortDescription: {
@@ -23,22 +23,32 @@ module.exports = {
 
   langs: {
     en: {
-      missing:
-        `✨ 𝗜𝗡𝗩𝗔𝗟𝗜𝗗 𝗨𝗥𝗟 ✨\n` +
-        `──────────────────\n` +
-        `⚠️ Please Provide A Valid Url\n` +
-        `📌 Example : webss https://example.com`,
-      loading:
-        `✨ 𝗦𝗖𝗥𝗘𝗘𝗡𝗦𝗛𝗢𝗧 ✨\n` +
-        `──────────────────\n` +
-        `📸 Web Screenshot Taking...\n` +
-        `🌐 %1`,
-      error:
-        `✨ 𝗦𝗖𝗥𝗘𝗘𝗡𝗦𝗛𝗢𝗧 𝗙𝗔𝗜𝗟𝗘𝗗 ✨\n` +
-        `──────────────────\n` +
-        `❌ Invalid Or Blocked Url\n\n` +
-        `──────────────────\n` +
-        `👑 𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 ➜ 𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍 👑`
+      missing: `━━━━━━━━━━━━━━━
+👑 𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍 👑
+━━━━━━━━━━━━━━━
+» 🐸 𝗜𝗡𝗩𝗔𝗟𝗜𝗗 𝗨𝗥𝗟 
+» ❤️‍🩹 𝗣𝗹𝗲𝗮𝘀𝗲 𝗣𝗿𝗼𝘃𝗶𝗱𝗲 𝗔 𝗩𝗮𝗹𝗶𝗱 𝗨𝗿𝗹
+» 🫣 𝗘𝘅𝗮𝗺𝗽𝗹𝗲 : 𝘄𝗲𝗯𝘀𝘀 𝗵𝘁𝘁𝗽𝘀://𝗲𝘅𝗮𝗺𝗽𝗹𝗲.𝗰𝗼𝗺
+━━━━━━━━━━━━━━━
+🧚‍♀️ 𝐍𝐈𝐉𝐇𝐔𝐌 𝐂𝐇𝐀𝐓𝐁𝐎𝐓
+━━━━━━━━━━━━━━━`,
+      loading: `━━━━━━━━━━━━━━━
+👑 𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍 👑
+━━━━━━━━━━━━━━━
+» 📸 𝗦𝗖𝗥𝗘𝗘𝗡𝗦𝗛𝗢𝗧 
+» ⏳ 𝗪𝗲𝗯 𝗦𝗰𝗿𝗲𝗲𝗻𝘀𝗵𝗼𝘁 𝗧𝗮𝗸𝗶𝗻𝗴...
+» 🌐 %1
+━━━━━━━━━━━━━━━
+🧚‍♀️ 𝐍𝐈𝐉𝐇𝐔𝐌 𝐂𝐇𝐀𝐓𝐁𝐎𝐓
+━━━━━━━━━━━━━━━`,
+      error: `━━━━━━━━━━━━━━━
+👑 𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍 👑
+━━━━━━━━━━━━━━━
+» ❌ 𝗦𝗖𝗥𝗘𝗘𝗡𝗦𝗛𝗢𝗧 𝗙𝗔𝗜𝗟𝗘𝗗 
+» ⚠️ 𝗜𝗻𝘃𝗮𝗹𝗶𝗱 𝗢𝗿 𝗕𝗹𝗼𝗰𝗸𝗲𝗱 𝗨𝗿𝗹
+━━━━━━━━━━━━━━━
+🧚‍♀️ 𝐍𝐈𝐉𝐇𝐔𝐌 𝐂𝐇𝐀𝐓𝐁𝐎𝐓
+━━━━━━━━━━━━━━━`
     }
   },
 
@@ -49,7 +59,7 @@ module.exports = {
       ? args[0]
       : `https://${args[0]}`;
 
-    const loadingMessage = getLang("loading").replace("%1", `**${url}**`);
+    const loadingMessage = getLang("loading").replace("%1", url);
     await message.reply(loadingMessage);
 
     try {
@@ -70,15 +80,17 @@ module.exports = {
 
       await message.reply(
         {
-          body:
-            `✨ 𝗦𝗖𝗥𝗘𝗘𝗡𝗦𝗛𝗢𝗧 𝗦𝗨𝗖𝗖𝗘𝗦𝗦 ✨\n` +
-            `──────────────────\n` +
-            `📸 Website Screenshot\n\n` +
-            `🌐 Url :  ${url}**\n` +
-            `🖼️ Type : Full Page\n` +
-            `⚡ Status : Success\n\n` +
-            `──────────────────\n` +
-            `👑 𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 ➜ 𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍 👑`,
+          body: `━━━━━━━━━━━━━━━
+👑 𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍 👑
+━━━━━━━━━━━━━━━
+» ✅ 𝗦𝗖𝗥𝗘𝗘𝗡𝗦𝗛𝗢𝗧 𝗦𝗨𝗖𝗖𝗘𝗦𝗦 
+» 📸 𝗪𝗲𝗯𝘀𝗶𝘁𝗲 𝗦𝗰𝗿𝗲𝗲𝗻𝘀𝗵𝗼𝘁
+» 🌐 𝗨𝗿𝗹 : ${url}
+» 🖼️ 𝗧𝘆𝗽𝗲 : 𝗙𝘂𝗹𝗹 𝗣𝗮𝗴𝗲
+» ⚡ 𝗦𝘁𝗮𝘁𝘂𝘀 : 𝗦𝘂𝗰𝗰𝗲𝘀𝘀
+━━━━━━━━━━━━━━━
+🧚‍♀️ 𝐍𝐈𝐉𝐇𝐔𝐌 𝐂𝐇𝐀𝐓𝐁𝐎𝐓
+━━━━━━━━━━━━━━━`,
           attachment: fs.createReadStream(filePath)
         },
         () => {
